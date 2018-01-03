a1356c50a17daa870a5aca29f593f345dace7dbe
Sergey Barsukov <barsukof@live.com>
2018-01-03 12:32:03 +0300

    Revert "more indices to the god of indexes"
    This reverts commit 310a086b8671d8bc5462f62821a8c475333d3368.

--- init.sql
+++ init.sql
@@ -49,11 +49,6 @@ DROP INDEX IF EXISTS index_on_users_nickname;
 CREATE UNIQUE INDEX index_on_users_nickname
   ON users (nickname);
 
-DROP INDEX IF EXISTS index_on_users_nickname_and_email;
-
-CREATE UNIQUE INDEX index_on_users_nickname_and_email
-  ON users (nickname, email);
-
 
 CREATE TABLE "posts" (
   "pID"      SERIAL  NOT NULL,
@@ -88,12 +83,7 @@ CREATE INDEX index_on_posts_path
 DROP INDEX IF EXISTS index_on_posts_id_and_path_and_thread;
 
 CREATE INDEX index_on_posts_id_and_path_and_thread
-  ON posts ("pID", thread, path);
-
-DROP INDEX IF EXISTS index_on_posts_id_and_path;
-
-CREATE INDEX index_on_posts_id_and_path
-  ON posts ("pID", path);
+  ON posts (path, "pID", thread);
 
 
 
